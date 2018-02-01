8e41b9b8b6e51d450128ae78f63fa135ee46e9ba
Harley Rayner <harleymteden@gmail.com>
2018-02-01 05:53:21 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -9,6 +9,8 @@
 # end
 
 def prime?(number)
-  (2..number-1).each {|n| return false if number <= 1 || number % n == 0}
-  return true
+  if number > 1
+    (2..number-1).each {|n| return false if number <= 1 || number % n == 0}
+    return true
+  end
 end
