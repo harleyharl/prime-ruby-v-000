f27c5e7fe1e0e90140575a116476e52b0d0ce6ca
Harley Rayner <harleymteden@gmail.com>
2018-02-01 05:51:46 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,14 @@
 # Add  code here!
-def prime?(number_to_test)
-    if number_to_test > 1 && number_to_test/number_to_test == 1
-      true
-    else
-      false
-    end
+# def prime?(number_to_test)
+#   # 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 
+#     if number_to_test > 1 && number_to_test/number_to_test == 1 && number_to_test % 2
+#       true
+#     else
+#       false
+#     end
+# end
+
+def prime?(number)
+  (2..number-1).each {|n| return false if number <= 1 || number % n == 0}
+  return true
 end
