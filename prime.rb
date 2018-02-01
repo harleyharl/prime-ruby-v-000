e8d0735a5924cb75227adbbd18ee67be4eac9d43
Harley Rayner <harleymteden@gmail.com>
2018-02-01 05:37:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,8 @@
 # Add  code here!
 def prime?(number_to_test)
     if number_to_test > 1 && number_to_test/number_to_test == 1
-    else false
-    end 
+      true
+    else
+      false
+    end
 end
